1e4d0006dbabe934c976f0e0c1ee79f3f8e613cc
Oliver Schrenk <oliver.schrenk@gmail.com>
2010-12-14 22:17:13 +0100

    Basic Layout

---
+++ js/sr.pad.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+	var width = 768;
+	var height = 1024/2;
+    var r = Raphael("canvas", width, height);
+	var rect = r.rect(0, 0, width, height);
+
+	// drawAttacker
+	
+	// drawTarget
+	
+	// draw option.picker
+	
+	// draw number.picker
+	
+	// draw slider
+	
+	// draw weapon.picker
+	
+	
+
+};
